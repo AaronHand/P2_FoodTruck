324278cbb5e08581f8c422c231937567f36a1c54
Tiana Greisel <tianagreisel@gmail.com>
2016-02-02 13:23:30 -0800

    Added comments to code, fixed a few bugs
    Put code up to PSR2 standards, added feedback to user if don't check a
    box or enter a box and don't enter a quantity

--- configFoodTruck.php
+++ configFoodTruck.php
@@ -25,10 +25,11 @@
 */
 function makeForm($array)
 {
-
-
+    //create a string to hold the form string to echo to the page
     $myReturn = '';
         
+        //add form tags to form string.  With method being post and have it be a postback form
+        //add table tages to format form with columns for selecting item, quantity, item, description, and price
         $myReturn .= '<form method="post" action="' . THIS_PAGE . '">
     
     <table border="1" style="width:50%">
@@ -40,9 +41,13 @@ function makeForm($array)
     <td>Price</td>
     </tr>';
     
+    //loop through array provided as parameter to the function that holds items for form to display
     foreach ($array as $item)
     {
-    
+        //add each item to form string with item name, description and price in separate columns
+        //create checkbox that stores the item name and input box for user to enter number of each
+        //item they want and store in an associate array name quantity that key is item name and value is
+        //quanity the user enters
         $myReturn .= 
         
     '<tr><td><input type="checkbox" name="itemType[]" value="' . $item->name . '"</td>
@@ -54,12 +59,13 @@ function makeForm($array)
     
     } 
     
+    //add submit button to form
     $myReturn .= '<tr><td></td><td></td><td></td><td></td><td><input type="submit" name="submit" value="Submit Order" /></td></tr></table></form>';   
-    return $myReturn;
     
-    
-    
+    //return form string with all items from array parameter added to the form
+    return $myReturn;  
 }//end makeForm()
+
 
 /**
 * This function calculate the total cost of a purchase based on the $itemsPurchased parameter
@@ -78,24 +84,33 @@ function makeForm($array)
 function calculateTotal($itemsPurchased, $itemsAvailable)
 {
         
-    
+    //loop through itemsPurchased array that hold the items the user purchased as key and quantity purchased as value
     foreach($itemsPurchased as $item => $quantity){
         
+        //loop through itemsAvailable array which is Item array that holds the items available that dynamically get added to form
         foreach ($itemsAvailable as $itm){
          
+         //if the item name in itemsAvailable array matches the item the user selected
          if ($itm->name == $item )
+             
+             //grab the item object from the itemsAvailable array
               $itemObject = $itm;
-                $price = $itemObject->price;
+                
+             //find the price of the array by selecting the price property of the item and store in price variable
+             $price = $itemObject->price;
             
                 
      }
+        //calculate the total of the purchase which is the price times quantity of each item purchased 
         $total += ($price * $quantity);
-        
+ 
     }
     
+    //return total
     return $total;
     
     }//end calculateTotal()
+
 
 /**
 * This function displays a form of an order purchased based on the $itemsPuchased parameter 
@@ -114,11 +129,14 @@ function calculateTotal($itemsPurchased, $itemsAvailable)
 **/
 function displayOrder($itemsPurchased, $itemsAvailable)
 {
+    //create a string to hold form that holds purchased order form
     $myReturn = '';
         
+    //use the calculateTotal() function to calculate the total of the order based on items user selected 
     $total = calculateTotal($itemsPurchased, $itemsAvailable);
     
-        $myReturn .= '
+    //add table tags to format form table, with quanity, item, price, and item total columns
+    $myReturn .= '
     <table border="1" style="width:50%">
     <tr>
     <td>Quantity</td>
@@ -127,27 +145,36 @@ function displayOrder($itemsPurchased, $itemsAvailable)
     <td>Item Total</td>
     </tr>';
     
+    //loop through the itemsPurchased array to get the items the user purchased and quantity of each item they want to purchase
     foreach ($itemsPurchased as $item => $quantity)
     {
         
+        //loop through itemsAvailable array which is Item array that holds the items available that dynamically get added to form
         foreach ($itemsAvailable as $itm){
          
+        //if the item name in itemsAvailable array matches the item the user selected
          if ($itm->name == $item )
+             
+             //grabe the item object from the itemsAvailable array
               $itemObject = $itm;
-                $price = $itemObject->price;
             
-        
-                
+             //find the price of the array by selecting the price property of the item and store in price variable
+             $price = $itemObject->price;          
      }
     
         //in $_POST[] even if user didn't pick, empty string.  This makes a 0 in quantity for better
         //output formatting in output table
         if ($quantity == "")
         {
-            
+            //display a 0 if user didn't enter a quantity for better output formatting
             $quantity = 0;
         }
+        
+        //create a subTotal array to store subtotal of each item (price * quantity)
         $subTotalArray[] = ($price * $quantity);
+        
+        //add quantity user selected, item name, price of each item, and subtotal of each item formatted to 2 decimal places
+        //to the purchased order form
         $myReturn .= 
         
     '<tr><td>' . $quantity . '</td>
@@ -158,19 +185,28 @@ function displayOrder($itemsPurchased, $itemsAvailable)
     </tr>';
     
     } 
+    
+    //create a subTotal variable to store the subTotal of the whole purchase, not just each item as subTotal did above
     $subTotal = 0;
+    
+    //loop through subTotalArray that stores the subTotal of each item
     foreach ($subTotalArray as $itemTotal){
         
+        //add the subtotal of each item to the subTotal of the entire order
         $subTotal += $itemTotal;
         
     }
+    
+    //calculate tax of the subTotal as 9.5% tax rate
     $tax = ($subTotal * .095);
+    
+    //add subTotal, tax, and total (subTotal plus tax) to the form columns
     $myReturn .= '<tr><td></td><td></td><td>Subtotal</td><td>$' . number_format($subTotal, 2) . '</td></tr>
                 <tr><td></td><td></td><td>Tax</td><td>$' . number_format($tax, 2) . '</td></tr>
         <tr><td></td><td></td><td>Total</td><td>$' . number_format(($total + $tax) , 2) . '</td></tr></table>';   
+    
+    //return the purchased order form string
     return $myReturn;
-    
-    
     
 }//end displayOrder()
     
--- foodTruck.php
+++ foodTruck.php
@@ -17,7 +17,7 @@
 * @todo none
 */
 
-include 'Item.php';  //include Item class to 
+include 'Item.php';  //include Item class to create item objects
 include 'configFoodTruck.php'; //contains all the functions for this application
 
 //create a constant named THIS_PAGE
@@ -38,21 +38,42 @@ if (isset($_POST['submit']))
     var_dump($_POST);
     echo '</pre>';*/
     
-  // $total = 0;
-    
-    
-    $itemsPurchased = $_POST['quantity'];
-       
-    foreach ($itemsPurchased as $input){
-    /*if (!is_numeric($input) && $input != ""){
-        echo "Please enter an integer value";
+    //check to make sure user selected checkbox of item to purchase
+    if (!isset($_POST['itemType'])){
         
-    }*/
-        //validate user input for each quantity box to make sure an integer number
+        //if they didn't check any item boxes, display message to select at least one item
+        echo "Please select at least one item to purchase";
+        
+        //reset $_POST data since 'submit' cached and need to reset to display form again 
+        unset($_POST);
+        
+        // display reset button so user can go back and enter different items
+        echo '
+    
+        <form method="post" action="' . THIS_PAGE . '">
+        <input type="submit" name="reset" value="reset" />
+    
+        </form>
+        ';
+            die;
+    
+    }
+    
+    //create a variable to store POST data of items user purchased.  Associative array with indix being item
+    //name and value is the quantity user purchased
+    $itemsPurchased = $_POST['quantity'];
+    
+    //create a variable to store POST data of items selected in checkboxes 
+    $itemsSelected = $_POST['itemType'];
+       
+    //loop through array of items user purchased, which is items they checked in checkboxes and quantity as input fields
+    foreach ($itemsPurchased as $item => $input){
+
+        //validate user input for each quantity input box to make sure an integer number
         //display feedback to the user
-        //if(!filter_var($input, FILTER_VALIDATE_INT) && $input != ""){
-          if((!is_numeric($input) && $input != "") || $input < 0){  
-            echo "Quantity must be an integer value";
+        if((!is_numeric($input) && $input != "") || $input < 0){ //if its not a number and not nothing, or the input is less than zero
+            echo "Quantity must be an positive integer value";   //display message to user saying quantity must be an integer value
+             
             //reset $_POST data since 'submit' cached and need to reset to display form again
             unset($_POST);
 
@@ -64,12 +85,37 @@ if (isset($_POST['submit']))
     
         </form>
         ';
-            die;
+            die;   
+        }  
+        
+        //if the user selected a item from checkbox, make sure entered a non-zero quantity value
+        if (isset($_POST['itemType'])){
+              
+            //loop through the itemsSelected checkbox array
+              foreach ($itemsSelected as $itmSelected)
+                    
+                  //if the itemSelected as checkbox matches quantity input value and the user didn't
+                  //enter a quantity or entered zero, display a message telling user to enter non-zero value
+                  if($itmSelected == $item && ($input == "" || $input == 0)){
+                      
+                      //display message to user to enter a non-zero quantity of selected item to purchase
+                      echo "Please enter a non-zero quantity of item to purchase";
+                      
+                      //reset $_POST data since 'submit' cached and need to reset to display form again
+                      unset($_POST);
+                      
+                     // display reset button so user can go back and enter different items
+                     echo '
     
-            
-        }
-        
-        
+                    <form method="post" action="' . THIS_PAGE . '">
+                    <input type="submit" name="reset" value="reset" />
+    
+                    </form>
+                    ';
+                    die;
+    
+                  }   
+    }
     }
     
     //call function displayOrder() to display the items the user picked and the total for their order
@@ -85,24 +131,16 @@ if (isset($_POST['submit']))
     <input type="submit" name="reset" value="reset" />
     
     </form>
-    ';
-    
-    
-    
+    ';    
 }
     
 else
 {
     
-
-    
-
 //call to makeForm() function using the items array that contains all the available items
-//makes a form dynamically based on the available items
+//makes a form dynamically based on the available items to display to user so they can select items to purchase
 echo makeForm($items);
- 
-
-       
+      
 }
 
 
